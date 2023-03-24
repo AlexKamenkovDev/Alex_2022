305f9f29dbab6b99579be613f7e31f2953f97d98
AlexKamenkovDev <akamenkov.dev@gmail.com>
2023-03-24 14:48:50 +0300

    hw_Django_Alex_2022_24.03.2023

--- Alex_2022/Alex_2022/urls.py
+++ Alex_2022/Alex_2022/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.about),
-    path('home/', views.home),
-    path('reverse/', views.reverse),
-    # path('', include('hello_world.urls'))
+    path('', include('project.urls'))
 ]
--- Alex_2022/Alex_2022/views.py
+++ Alex_2022/Alex_2022/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 
 
 def about(request):
-    return render(request, 'about.html')
+    a = 5 + 6
+    return render(request, 'about.html', {'greeting': a})
 
 
 def reverse(request):
-    # user_text = request.GET['username']
-    # reversed_text = user_text[::-1]
-    return render(request, 'reverse.html')
+    user_text = request.GET['username']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'word': reversed_text})
 
 
 def home(request):
