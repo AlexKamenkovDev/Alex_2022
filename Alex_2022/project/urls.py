ac5b7831c7d500a3adb9cd5fd475656fc171eb86
AlexKamenkovDev <akamenkov.dev@gmail.com>
2023-03-25 17:06:29 +0300

    hw_Django_Alex_2022_25.03.2023

--- Alex_2022/project/urls.py
+++ Alex_2022/project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.project_index, name="project_index"),
+    path("<int:pc>/", views.project_detail, name="project_detail")
 ]
--- Alex_2022/project/views.py
+++ Alex_2022/project/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Project
+
 
 # Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pc):
+    project = Project.objects.get(pc=pc)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
